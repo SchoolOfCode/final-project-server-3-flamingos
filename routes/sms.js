3cf0fb75a28eee6c213ec4fe0758b13a978884a6
Bukola <bukola_jimoh@yahoo.co.uk>
2019-04-30 17:05:44 +0100

    update .env file

--- routes/sms.js
+++ routes/sms.js
@@ -5,6 +5,7 @@ const twilio = require("twilio");
 
 var accountSid = process.env.TWILIO_ACCOUNTSID; // Your Account SID from www.twilio.com/console
 var authToken = process.env.AUTH_TOKEN; // Your Auth Token from www.twilio.com/console
+var twilioNumber = process.env.TWILIO_NUMBER;
 
 //var twilio = require("twilio");
 var client = new twilio(accountSid, authToken);
@@ -15,7 +16,7 @@ router.get("/:id", (req, res, next) => {
     .create({
       body: `Hello from 3-flamingos! https://github.com/orgs/SchoolOfCode/teams/3-flamingos`,
       to: `${phoneNumber}`, // Text this number
-      from: "+447588688297" // From a valid Twilio number
+      from: `${twilioNumber}` // From a valid Twilio number
     })
     .then(message => console.log(message.sid));
 
